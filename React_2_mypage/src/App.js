ae6b4aec74ef98567e6ad3ab4fd914aff3cc7d0d
HyunSangHan <gustkd3@gmail.com>
2019-01-26 12:53:15 +0900

    190126 Hyunsang - arrange folders

---
+++ React_2_mypage/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <div className="my-header">Header</div>
+          <div>body</div>
+          <div>footer</div>
+      </div>
+    );
+  }
+}
+
+export default App;
