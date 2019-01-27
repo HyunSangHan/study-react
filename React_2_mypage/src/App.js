1ccbaa4f8ca0d718e41328928e9976f553bd83c6
HyunSangHan <gustkd3@gmail.com>
2019-01-28 01:23:53 +0900

    190128_1 Hyunsang - start making mypage

--- React_2_mypage/src/App.js
+++ React_2_mypage/src/App.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import menu from './menu_icon.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <div className="my-header">Header</div>
-          <div>body</div>
-          <div>footer</div>
+          <div className="header-common">
+              <div className="header-logo ml-3">
+                  <img src ={logo} alt = "home" height={"40px;"}/>
+              </div>
+              <div className="header-menus mr-3">
+                  <div>ABOUT</div>
+                  <div>TIMELINE</div>
+                  <div>SKILL SET</div>
+                  <div>PRODUCT</div>
+                  <div>CONTACT</div>
+                  <div>GALLERY</div>
+              </div>
+              <div className="header-hamburger mr-3">
+                  <img src = "menu_icon.png" alt = "menu"/>
+              </div>
+          </div>
+          <div className="body-common">
+              <div className="gate">큰 전광판 부분</div>
+              <div className="about">
+                  <div className="about-left">ABOUT부분1</div>
+                  <div className="about-right">ABOUT부분2</div>
+              </div>
+              <div className="rest">TIMELINE부분</div>
+              <div className="rest">SKILL SET부분</div>
+              <div className="rest">PRODUCT부분</div>
+              <div className="rest">CONTACT부분</div>
+              <div className="rest">GALLERY부분</div>
+          </div>
+          <div className="footer-common">
+              <div>Copyright © 2019 Hyunsang Han</div>
+          </div>
       </div>
     );
   }
