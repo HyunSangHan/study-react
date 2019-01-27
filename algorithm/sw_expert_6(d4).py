3d2166f3b47a62a2a514a96d3f04495ae79270af
HyunSangHan <gustkd3@gmail.com>
2019-01-28 01:50:32 +0900

    190128_1 H2 hyunsang - Edit: algorithm 6

--- algorithm/sw_expert_6(d4).py
+++ algorithm/sw_expert_6(d4).py
@@ -1,16 +1,16 @@
 def miro(arr, visited, m, n):
     global ans
-    if(arr[m][n] == 3):
+    if (arr[m][n] == 3):
         ans = 1
         return
-    if(arr[m][n] == 1 or visited[m][n] == 1):
+    if (arr[m][n] == 1 or visited[m][n] == 1):
         return
     visited[m][n] = 1
-    miro(arr, visited, m+1, n)
-    miro(arr, visited, m, n+1)
-    miro(arr, visited, m-1, n)
-    miro(arr, visited, m, n-1)
-    
+    miro(arr, visited, m + 1, n)
+    miro(arr, visited, m, n + 1)
+    miro(arr, visited, m - 1, n)
+    miro(arr, visited, m, n - 1)
+
 BYNUM = 100
 ans = -1
 test_cases = range(10)
@@ -21,11 +21,11 @@ for test_case in test_cases:
     is_visited = []
     tmp = []
     for a in range(BYNUM):
-        for b in range(BYNUM):
-            tmp.append(0)
+        tmp.append(0)
+    for a in range(BYNUM):
         array.append(list(map(int, input())))
         is_visited.append(tmp)
 
     miro(array, is_visited, 1, 1)
-    print("#{} {}".format(test_case+1, ans))
-    ans = -1
+    print("#{} {}".format(test_case + 1, ans))
+    ans = -1 #it in not the answer if ans == -1.
