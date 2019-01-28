4661e53b3056f825d0de868df0a37b5b5724490d
HyunSangHan <gustkd3@gmail.com>
2019-01-28 13:43:49 +0900

    190128_4 Hyunsang - Edit: algorithm 7

--- algorithm/sw_expert_7(d4).py
+++ algorithm/sw_expert_7(d4).py
@@ -1,7 +1,21 @@
-#def
+def convert(num, l):
+    converted = []
+    for i in range(100):
+        converted.append([])
+    for j in range(l):
+        if j % 2 == 1:
+            converted[num[j-1]].extend(num[j])
+    return converted
 
-T = int(input())
+#def result(converted):
+#    global ans
 
-for n in range(T):
-    tmp = list(map(int, input()))
-    
+#ans = -1
+
+level, length = input().split()
+nums = list(map(int, input().split())) #making list of int
+
+#print("#{} {}".format(level, res))
+#ans = -1
+
+print(convert(nums, length)) #this is for test
