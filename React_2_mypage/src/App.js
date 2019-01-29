09927287a14b347ec05726c6385e13b9c6bbeafc
HyunSangHan <gustkd3@gmail.com>
2019-01-29 22:05:48 +0900

    190129 Hyunsang - Try: using Bootstrap

--- React_2_mypage/src/App.js
+++ React_2_mypage/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import menu from './menu_icon.svg';
 import './App.css';
+import {Grid, Row, Col, Button} from 'react-bootstrap';
+import MaterialIcon, {colorPalette} from 'material-icons-react';
+import ButtonToolbar from "react-bootstrap/es/ButtonToolbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <div className="header-common">
+          <div className="header-common w-limit font2">
               <div className="header-logo ml-3">
                   <img src ={logo} alt = "home" height={"40px;"}/>
               </div>
@@ -19,23 +21,61 @@ class App extends Component {
                   <div>CONTACT</div>
                   <div>GALLERY</div>
               </div>
+
               <div className="header-hamburger mr-3">
-                  <img src = "menu_icon.png" alt = "menu"/>
+                  <ButtonToolbar>
+                      <Button bsSize="large" blocks>
+                          <MaterialIcon icon="menu" size="medium" color="white"/>
+                      </Button>
+                  </ButtonToolbar>
               </div>
           </div>
           <div className="body-common">
-              <div className="gate">큰 전광판 부분</div>
-              <div className="about">
-                  <div className="about-left">ABOUT부분1</div>
-                  <div className="about-right">ABOUT부분2</div>
+              <div className="gate font4">
+                  이 페이지는 아직 공사중입니다.
               </div>
-              <div className="rest">TIMELINE부분</div>
-              <div className="rest">SKILL SET부분</div>
-              <div className="rest">PRODUCT부분</div>
-              <div className="rest">CONTACT부분</div>
-              <div className="rest">GALLERY부분</div>
+              <div className="about font2">
+                  <Grid>
+                      <Row className="h100percent">
+                          <Col className="about-each rest" xs={12} md={4}>
+                              ABOUT부분1
+                          </Col>
+                          <Col className="about-each rest" xs={12} md={8}>
+                              ABOUT부분2
+                          </Col>
+                      </Row>
+
+                  </Grid>
+              </div>
+              <Grid>
+                  <Row className="h100percent">
+                      <Col className="about-each rest font2" xs={12} md={12}>
+                          TIMELINE부분
+                      </Col>
+                  </Row>
+                  <Row className="h100percent">
+                      <Col className="about-each rest font2" xs={12} md={12}>
+                          스킬셋
+                      </Col>
+                  </Row>
+                  <Row className="h100percent">
+                      <Col className="about-each rest font2" xs={12} md={12}>
+                          프로덕트
+                      </Col>
+                  </Row>
+                  <Row className="h100percent">
+                      <Col className="about-each rest font2" xs={12} md={12}>
+                          컨택
+                      </Col>
+                  </Row>
+                  <Row className="h100percent">
+                      <Col className="about-each rest font2" xs={12} md={12}>
+                          갤러리
+                      </Col>
+                  </Row>
+              </Grid>
           </div>
-          <div className="footer-common">
+          <div className="footer-common font1">
               <div>Copyright © 2019 Hyunsang Han</div>
           </div>
       </div>
