e4d77e33ff16f49c4a16330308b3f4556cd23ebf
HyunSangHan <gustkd3@gmail.com>
2019-01-30 02:36:50 +0900

    190130_1 Hyunsang - Input: some ABOUT contents example

--- React_2_mypage/src/App.js
+++ React_2_mypage/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <div className="header-common w-limit font2">
+          <div className="header-common font-2 z-4">
               <div className="header-logo ml-3">
                   <img src ={logo} alt = "home" height={"40px;"}/>
               </div>
@@ -30,52 +30,66 @@ class App extends Component {
                   </ButtonToolbar>
               </div>
           </div>
-          <div className="body-common">
-              <div className="gate font4">
+
+          <div className="body-common z-0">
+              <div className="body-under font-4 z-1">
                   이 페이지는 아직 공사중입니다.
+                  {/*<img src="../images/Python.png" />*/}
               </div>
-              <div className="about font2">
+              <div className="body-on font-2 z-3">
+                  <div className="font-4">
+                      <u>ABOUT</u>
+                  </div>
                   <Grid>
                       <Row className="h100percent">
-                          <Col className="about-each rest" xs={12} md={4}>
-                              ABOUT부분1
+                          <Col xs={12} md={5} mdOffset={1}>
+                              <div className="mt-5">
+                                  <img className="photo" src="../images/Me.jpg" />
+                              </div>
                           </Col>
-                          <Col className="about-each rest" xs={12} md={8}>
-                              ABOUT부분2
+                          <Col className="mt-5" xs={11} xsOffset={1} md={6}>
+                              <div className="mt-5 align-left">
+                                <h3>한현상</h3>
+                                  <h4 className="upper mt-3 mb-5">hyunsang han</h4>
+                                  <p>네이버에 재직중</p><p>블라블라</p>
+                              </div>
                           </Col>
                       </Row>
-
+                      <Row className="h100percent">
+                          <Col className="timeline" xs={6} md={4}>
+                              TIMELINE 설명1(왼쪽)
+                          </Col>
+                          <Col className="timeline" xs={6} md={4}>
+                              TIMELINE 가운데
+                          </Col>
+                          <Col className="timeline" xs={6} md={4}>
+                              TIMELINE 설명2(오른쪽)
+                          </Col>
+                      </Row>
+                      <Row className="h100percent">
+                          <Col className="rest" xs={12} md={12}>
+                              스킬셋
+                          </Col>
+                      </Row>
+                      <Row className="h100percent">
+                          <Col className="rest" xs={12} md={12}>
+                              프로덕트
+                          </Col>
+                      </Row>
+                      <Row className="h100percent">
+                          <Col className="rest" xs={12} md={12}>
+                              컨택
+                          </Col>
+                      </Row>
+                      <Row className="h100percent">
+                          <Col className="rest" xs={12} md={12}>
+                              갤러리
+                          </Col>
+                      </Row>
                   </Grid>
               </div>
-              <Grid>
-                  <Row className="h100percent">
-                      <Col className="about-each rest font2" xs={12} md={12}>
-                          TIMELINE부분
-                      </Col>
-                  </Row>
-                  <Row className="h100percent">
-                      <Col className="about-each rest font2" xs={12} md={12}>
-                          스킬셋
-                      </Col>
-                  </Row>
-                  <Row className="h100percent">
-                      <Col className="about-each rest font2" xs={12} md={12}>
-                          프로덕트
-                      </Col>
-                  </Row>
-                  <Row className="h100percent">
-                      <Col className="about-each rest font2" xs={12} md={12}>
-                          컨택
-                      </Col>
-                  </Row>
-                  <Row className="h100percent">
-                      <Col className="about-each rest font2" xs={12} md={12}>
-                          갤러리
-                      </Col>
-                  </Row>
-              </Grid>
           </div>
-          <div className="footer-common font1">
+          <div className="footer-common font1 z-3">
               <div>Copyright © 2019 Hyunsang Han</div>
           </div>
       </div>
