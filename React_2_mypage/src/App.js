238892262a5e4c8fc2a4433a10867cecb86e9cd6
HyunSangHan <gustkd3@gmail.com>
2019-02-04 17:23:46 +0900

    190204_3 Hyunsang - Error: TypeError: Cannot read property location of undefined

--- React_2_mypage/src/App.js
+++ React_2_mypage/src/App.js
@@ -4,17 +4,23 @@ import {colorPalette} from 'material-icons-react';
 import Header from './views/Header.js';
 import Footer from './views/Footer.js';
 import Main from './views/Main.js';
+import { BrowserRouter, Route, Router, Link } from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Header/>
-          <Main/>
-          <Footer/>
-       </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <Header/>
+                    <Router>
+                        <Route path="/" component={Main}/>
+                        {/*<Main/>*/}
+                    </Router>
+                    <Footer/>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
