6dd00dd4a7751e2e4e2d09fdf7e5ecdbbd7f00ae
HyunSangHan <gustkd3@gmail.com>
2019-02-06 22:04:43 +0900

    190206_5 Hyunsang - Add: contact mailing

---
+++ React_2_mypage/client/src/views/GalleryHeader.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../App.css';
+import '../css/Header.css'
+import '../css/Main.css'
+import {Link} from "react-router-dom";
+
+class GalleryHeader extends Component {
+    render() {
+        return (
+            <div className="header-common z-4">
+                <div className="header-logo ml-3 z-4">
+                    <Link to = "/">
+                        <img src ="../images/arrowback.png" alt = "back" height={"40px;"} color={"white"} />
+                    </Link>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default GalleryHeader;
---
+++ React_2_mypage/index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post('/api/form', (req, res) => {
+    nodemailer.createTestAccount((err, account) => {
+        const htmlEmail = `
+        <h3>Contact Details</h3>
+        <ul>
+            <li>Name: ${req.body.name}</li>
+            <li>Email: ${req.body.email}</li>
+            <li>Phone: ${req.body.phone}</li>
+        </ul>
+        <h3>Message</h3>
+        <p>${req.body.message}</p>
+        `
+
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: 'dtw2h24xhecvjd3z@ethereal.email',
+                pass: 'MADRGreeCH5J5jFgjz'
+            }
+        })
+
+        let mailOptions = {
+            from: 'test@testaccunt.com',
+            to: 'dtw2h24xhecvjd3z@ethereal.email',
+            replyTo: 'test@testaccunt.com',
+            subject: 'My Page - New Message',
+            text: req.body.message,
+            html: htmlEmail
+
+        }
+
+        transporter.sendMail(mailOptions, (err,info) => {
+            if (err) {
+                return console.log(err)
+            }
+
+            console.log('Message sent: %s', info.message)
+            console.log('Message URL: %s', nodemailer.getTestMessageUrl(info))
+        })
+    })
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
