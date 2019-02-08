e098feff53b4c6dda5523858cb40a212ecfb8f15
HyunSangHan <gustkd3@gmail.com>
2019-02-09 00:16:39 +0900

    190209_3 Hyunsang - Edit: minor parts

--- React_2_mypage/client/src/views/GalleryHeader.js
+++ React_2_mypage/client/src/views/GalleryHeader.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import '../App.css';
 import '../css/Header.css'
 import '../css/Main.css'
+import {Link} from "react-router-dom";
 
 class GalleryHeader extends Component {
     render() {
         return (
             <div className="header-common z-4">
                 <div className="header-logo ml-3 z-4">
-                    <a href = "/" target={"_self"}>
+                    <Link to = "/">
                         <img src ="../images/arrowback.png" alt = "back" height={"40px;"} color={"white"} />
-                    </a>
+                    </Link>
                 </div>
                 <div className={"upper g-header"}>Gallery</div>
             </div>
