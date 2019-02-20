33d92312b65132b0cba2e5da8be83f973b0f2da0
HyunSangHan <gustkd3@gmail.com>
2019-02-20 20:38:42 +0900

    190220_1 Hyunsang - Make: directories and files

---
+++ React_3/src/Actions/index.js
@@ -0,0 +1,10 @@
+import * as types from './ActionTypes';
+
+export const increment = () => ({
+    type: types.INCREMENT;
+})
+
+export const setColor = (color) => ({
+    type: types.SET_COLOR;
+    color
+})
