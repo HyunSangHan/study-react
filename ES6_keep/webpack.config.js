c5024b97f28247354f0f6d42a058e3c5baacf9c3
HyunSangHan <gustkd3@gmail.com>
2019-09-01 00:00:59 +0900

    190901 Add: babel setting

--- ES6_keep/webpack.config.js
+++ ES6_keep/webpack.config.js
@@ -3,11 +3,26 @@ var path = require('path');
 module.exports = {
   entry: './src/index.js',
   output : {
-  filename: 'bundle.js',
-  path: path.resolve(__dirname, 'list')
+  filename : 'bundle.js',
+  path : path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [{
+  module : {
+    rules : [{
+      test : /\.js$/,
+      include : path.resolve(__dirname, 'src'),
+      use : {
+        loader : 'babel-loader',
+        options : {
+          presets : [
+            ['env', {
+              'targets' : {
+                'browsers' : ["last 2 versions", "ie 9"]
+              },
+              'debug' : true
+            }]
+          ]
+        }
+      }
     }]
   }
 }
