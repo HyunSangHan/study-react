b30f56f86575070726dd5dc273317d23a1f66bc8
KyoungWan <coruddhks@gmail.com>
2019-10-28 19:55:59 +0900

    Change variable name as suitable

--- ES6_minesweeper/game.js
+++ ES6_minesweeper/game.js
@@ -1,7 +1,7 @@
 const board = document.getElementById("board");
 const cells = [];
-let col = null;
-let row = null;
+let colNumber = null;
+let rowNumber = null;
 let mine = null;
 let isOver = false;
 let cellsLeft = null;
@@ -10,46 +10,42 @@ let cellsLeft = null;
 document.getElementById("play").addEventListener('click', function(){
     reset();
     // 정수화하기
-    col = parseInt(document.getElementById("col").value);
-    row = parseInt(document.getElementById("row").value);
+    colNumber = parseInt(document.getElementById("col").value);
+    rowNumber = parseInt(document.getElementById("row").value);
     mine = parseInt(document.getElementById("mine").value);
-    if (col < 1 || row < 1 || mine < 1) { //입력값이 올바르지 않을 때
+    if (colNumber < 1 || rowNumber < 1 || mine < 1) { //입력값이 올바르지 않을 때
         alert('입력값이 올바르지 않습니다.');
-    } else if (col * row <= mine) { //지뢰가 칸 개수보다 많거나 같을 때
+    } else if (colNumber * rowNumber <= mine) { //지뢰가 칸 개수보다 많거나 같을 때
         alert('지뢰가 너무 많아서 게임이 불가능해요. 지뢰 개수를 줄여주세요.');
-    } else if (col > 30 || row > 30) {
+    } else if (colNumber > 30 || rowNumber > 30) {
         alert('언제 다 하시려고... 30 X 30 미만으로 줄여주세요.');
     } else { //일반적인 경우(게임 가능)
-        // board 중복 방지
-        while (board.hasChildNodes()) {
-            board.removeChild(board.firstChild);
-        }
-        document.getElementById("cellsLeft").innerText = col * row;
-        for(let i = 0; i < row; i++) {
+        document.getElementById("cellsLeft").innerText = colNumber * rowNumber;
+        for(let i = 0; i < rowNumber; i++) {
             const row = []; // 하나의 행
             cells.push(row); // 빈 행을 여러 열에 넣음
-            const domElementParent = document.createElement('div'); // 한세트 칸 생성
-            domElementParent.classList.add('cellParent');
+            const rowElement = document.createElement('div'); // 한세트 칸 생성
+            rowElement.classList.add('row');
 
-            board.appendChild(domElementParent); // DOM에 칸 그리기
+            board.appendChild(rowElement); // DOM에 칸 그리기
 
-            for(let j=0; j < col; j++) {
-                const domElement = document.createElement('div'); // 하나의 칸 생성
-                domElementParent.appendChild(domElement); // DOM에 칸 그리기
+            for(let j=0; j < colNumber; j++) {
+                const cellElement = document.createElement('div'); // 하나의 칸 생성
+                rowElement.appendChild(cellElement); // DOM에 칸 그리기
 
                 // 그 칸의 속성들 설정하기
-                domElement.classList.add('cell');
-                domElement.classList.add('close');
-                domElement.setAttribute('y', i);
-                domElement.setAttribute('x', j);
-                row[j] = domElement;
+                cellElement.classList.add('cell');
+                cellElement.classList.add('close');
+                cellElement.setAttribute('y', i);
+                cellElement.setAttribute('x', j);
+                row[j] = cellElement;
 
-                domElement.addEventListener('click', function(e) {
+                cellElement.addEventListener('click', function(e) {
                     e.preventDefault();
                     openCell(this);
                 });
 
-                domElement.addEventListener('contextmenu', function(e) {
+                cellElement.addEventListener('contextmenu', function(e) {
                     e.preventDefault();
                     flagCell(this);
                 });
@@ -112,7 +108,7 @@ const getNeighborCells = function(cell) {
     const y = parseInt(cell.getAttribute('y'));
     const neighborCells = [ cells[y][x] ];
 
-    if (0 < y && y < row-1 && 0 < x && x < col-1) {
+    if (0 < y && y < rowNumber-1 && 0 < x && x < colNumber-1) {
         neighborCells.push(
             cells[y+1][x+1],
             cells[y+1][x],
@@ -123,7 +119,7 @@ const getNeighborCells = function(cell) {
             cells[y-1][x],
             cells[y-1][x-1]
         )
-    } else if (y === 0 && 0 < x && x < col-1) {
+    } else if (y === 0 && 0 < x && x < colNumber-1) {
         neighborCells.push(
             cells[y+1][x+1],
             cells[y+1][x],
@@ -131,13 +127,13 @@ const getNeighborCells = function(cell) {
             cells[y][x+1],
             cells[y][x-1]
         )
-    } else if (y === 0 && x === col-1) {
+    } else if (y === 0 && x === colNumber-1) {
         neighborCells.push(
             cells[y+1][x],
             cells[y+1][x-1],
             cells[y][x-1],
         )
-    } else if (0 < y && y < row-1 && x === col-1) {
+    } else if (0 < y && y < rowNumber-1 && x === colNumber-1) {
         neighborCells.push(
             cells[y+1][x],
             cells[y+1][x-1],
@@ -145,13 +141,13 @@ const getNeighborCells = function(cell) {
             cells[y-1][x],
             cells[y-1][x-1]
         )
-    } else if (y === row-1 && 0 < x && x === col-1) {
+    } else if (y === rowNumber-1 && 0 < x && x === colNumber-1) {
         neighborCells.push(
             cells[y][x-1],
             cells[y-1][x],
             cells[y-1][x-1]
         )
-    } else if (y === row-1 && 0 < x && x < col-1) {
+    } else if (y === rowNumber-1 && 0 < x && x < colNumber-1) {
         neighborCells.push(
             cells[y][x+1],
             cells[y][x-1],
@@ -159,13 +155,13 @@ const getNeighborCells = function(cell) {
             cells[y-1][x],
             cells[y-1][x-1]
         )
-    } else if (y === row-1 && x === 0) {
+    } else if (y === rowNumber-1 && x === 0) {
         neighborCells.push(
             cells[y][x+1],
             cells[y-1][x+1],
             cells[y-1][x],
         )
-    } else if (0 < y && y < row-1 && x === 0) {
+    } else if (0 < y && y < rowNumber-1 && x === 0) {
         neighborCells.push(
             cells[y+1][x+1],
             cells[y+1][x],
@@ -188,7 +184,7 @@ const getNeighborCells = function(cell) {
 
 function makeMine() {
     let i, j, k;
-    const uniqueNumbers = [...Array(row * col).keys()];
+    const uniqueNumbers = [...Array(rowNumber * colNumber).keys()];
     for (i = uniqueNumbers.length; i; i -= 1) {
         j = Math.floor(Math.random() * i);
         k = uniqueNumbers[i - 1];
@@ -197,8 +193,8 @@ function makeMine() {
     }
 
     for (let count = 0; count < mine; count++) { // 지뢰 다 깔 때까지 반복
-        const x = parseInt(uniqueNumbers[0] / row);
-        const y = uniqueNumbers[0] % row;
+        const x = parseInt(uniqueNumbers[0] / rowNumber);
+        const y = uniqueNumbers[0] % rowNumber;
         uniqueNumbers.shift();
         cells[y][x].setAttribute('isMine', true);
     }
@@ -206,8 +202,8 @@ function makeMine() {
 
 function gameOver() {
     alert('으악! 지뢰를 밟았다!');
-    for(let y = 0; y < row; y++) {
-        for(let x = 0; x < col; x++) {
+    for(let y = 0; y < rowNumber; y++) {
+        for(let x = 0; x < colNumber; x++) {
             if (cells[y][x].getAttribute('isMine') === 'true') {
                 cells[y][x].classList.remove('flag')
                 cells[y][x].classList.add('mine')
@@ -230,8 +226,8 @@ function reset() {
         board.removeChild(board.firstChild);
     }
     cells.splice(0, cells.length);
-    col = null;
-    row = null;
+    colNumber = null;
+    rowNumber = null;
     mine = null;
     isOver = false;
     cellsLeft = null;
