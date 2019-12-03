27edd34cbec7f15172aa0462e452b264a8f0e60c
HyunSangHan <gustkd3@gmail.com>
2019-12-03 09:03:12 +0900

    Import ListGroup from reactstrap

--- React_2_mypage/client/src/views/Contribution.js
+++ React_2_mypage/client/src/views/Contribution.js
@@ -4,14 +4,11 @@ import "../css/Main.css"
 import { Grid, Row, Col } from "react-bootstrap"
 import ScrollableAnchor from "react-scrollable-anchor"
 import {
-  Card,
-  CardBody,
-  CardLink,
-  CardSubtitle,
-  CardText,
-  CardTitle,
-  Collapse,
-  Button
+  ListGroup,
+  ListGroupItem,
+  ListGroupItemHeading,
+  ListGroupItemText,
+  Badge
 } from "reactstrap"
 
 class Contribution extends Component {
@@ -29,8 +26,68 @@ class Contribution extends Component {
             </div>
             <Grid>
               <Row className="h100percent mt-9">
-                <Col xs={12} md={6} lg={4} xl={3}>
-                  오픈소스 컨트리뷰션
+                <Col xs={12} md={12} lg={6} xl={6}>
+                  <ListGroup className="mt-3">
+                    <ListGroupItem style={{ backgroundColor: "#40c5ef" }}>
+                      <ListGroupItemHeading>
+                        List group item heading
+                      </ListGroupItemHeading>
+                      <ListGroupItemText>
+                        Donec id elit non mi porta gravida at eget metus.
+                        Maecenas sed diam eget risus varius blandit.
+                        <Badge color="primary">Primary</Badge>
+                      </ListGroupItemText>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      <ListGroupItemHeading>
+                        List group item heading
+                      </ListGroupItemHeading>
+                      <ListGroupItemText>
+                        Donec id elit non mi porta gravida at eget metus.
+                        Maecenas sed diam eget risus varius blandit.
+                      </ListGroupItemText>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      <ListGroupItemHeading>
+                        List group item heading
+                      </ListGroupItemHeading>
+                      <ListGroupItemText>
+                        Donec id elit non mi porta gravida at eget metus.
+                        Maecenas sed diam eget risus varius blandit.
+                      </ListGroupItemText>
+                    </ListGroupItem>
+                  </ListGroup>{" "}
+                </Col>
+                <Col xs={12} md={12} lg={6} xl={6}>
+                  <ListGroup className="mt-3">
+                    <ListGroupItem style={{ backgroundColor: "#40c5ef" }}>
+                      <ListGroupItemHeading>
+                        List group item heading
+                      </ListGroupItemHeading>
+                      <ListGroupItemText>
+                        Donec id elit non mi porta gravida at eget metus.
+                        Maecenas sed diam eget risus varius blandit.
+                      </ListGroupItemText>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      <ListGroupItemHeading>
+                        List group item heading
+                      </ListGroupItemHeading>
+                      <ListGroupItemText>
+                        Donec id elit non mi porta gravida at eget metus.
+                        Maecenas sed diam eget risus varius blandit.
+                      </ListGroupItemText>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      <ListGroupItemHeading>
+                        List group item heading
+                      </ListGroupItemHeading>
+                      <ListGroupItemText>
+                        Donec id elit non mi porta gravida at eget metus.
+                        Maecenas sed diam eget risus varius blandit.
+                      </ListGroupItemText>
+                    </ListGroupItem>
+                  </ListGroup>{" "}
                 </Col>
               </Row>
             </Grid>
