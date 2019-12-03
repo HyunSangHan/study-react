7e5b51855985f9983cb0f350467e22be9c7abedb
HyunSangHan <gustkd3@gmail.com>
2019-12-04 01:58:13 +0900

    Improve structure of contribution component

--- React_2_mypage/client/src/views/Contribution.js
+++ React_2_mypage/client/src/views/Contribution.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import "../App.css"
 import "../css/Main.css"
+import styled, { css } from "styled-components"
 import { Grid, Row, Col } from "react-bootstrap"
 import ScrollableAnchor from "react-scrollable-anchor"
 import {
@@ -10,7 +11,6 @@ import {
   ListGroupItemText,
   Badge
 } from "reactstrap"
-import styled from "styled-components"
 
 const ListGroupItemCustomTitle = styled(ListGroupItem)`
   border: 2px solid #fafafa !important;
@@ -34,110 +34,143 @@ const ListGroupItemCustomContent = styled(ListGroupItem)`
   }
 `
 
-const BadgeMerged = styled(Badge)`
+const BadgeCustom = styled(Badge)`
   margin-left: 4px !important;
-  background-color: #6f42c1 !important;
-  color: #fff !important;
+
+  ${props => {
+    props.type === "Merged" &&
+    css`
+      background-color: #6f42c1 !important;
+      color: #fff !important;
+    `(props.type === "Open") &&
+    css`
+      background-color: #2cbe4e !important;
+      color: #fff !important;
+    `(props.type === "Closed") &&
+    css`
+      background-color: #cb2431 !important;
+      color: #fff !important;
+    `(props.type === "Appoved") && //TODO:
+    css`
+      background-color: #cb2431 !important;
+      color: #fff !important;
+    `(props.type === "Released") && //TODO:
+      css`
+        background-color: #cb2431 !important;
+        color: #fff !important;
+      `
+  }}
 `
 
-const BadgeOpen = styled(Badge)`
-  margin-left: 4px !important;
-  background-color: #2cbe4e !important;
-  color: #fff !important;
-`
-
-const BadgeClosed = styled(Badge)`
-  margin-left: 4px !important;
-  background-color: #cb2431 !important;
-  color: #fff !important;
-`
-
-class Contribution extends Component {
-  render() {
-    return (
-      <>
-        <div className="bg-grey pb-9">
-          <div className="bg-grey">
-            <div className="title-out pt-4 pb-4">
-              <div className="title-except">
-                <ScrollableAnchor id={"contribution"}>
-                  <h1 className="font-5 font-new">contribution</h1>
-                </ScrollableAnchor>
-              </div>
-            </div>
-            <Grid>
-              <Row className="h100percent mt-9">
-                <Col xs={12} md={12} lg={6} xl={6}>
-                  <ListGroup className="mt-3">
-                    <ListGroupItemCustomTitle>
-                      <ListGroupItemHeading>MochaJs</ListGroupItemHeading>
-                    </ListGroupItemCustomTitle>
-                    <ListGroupItemCustomContent>
-                      <ListGroupItemHeading>
-                        Add a description for installing GraphicsMagick or
-                        ImageMagick in docs/README.md
-                        <BadgeMerged>Merged</BadgeMerged>
-                      </ListGroupItemHeading>
-                      <ListGroupItemText>
-                        <a
-                          href="https://github.com/mochajs/mocha/pull/4045"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          view it on github
-                        </a>
-                      </ListGroupItemText>
-                    </ListGroupItemCustomContent>
-                    <ListGroupItemCustomContent>
-                      <ListGroupItemHeading>
-                        Change sh to bash for code block in docs/index.md
-                        <BadgeMerged>Merged</BadgeMerged>
-                      </ListGroupItemHeading>
-                      <ListGroupItemText>
-                        <a
-                          href="https://github.com/mochajs/mocha/pull/4066"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          view it on github
-                        </a>
-                      </ListGroupItemText>
-                    </ListGroupItemCustomContent>
-                  </ListGroup>
-                  &nbsp;
-                </Col>
-                <Col xs={12} md={12} lg={6} xl={6}>
-                  <ListGroup className="mt-3">
-                    <ListGroupItemCustomTitle>
-                      <ListGroupItemHeading>
-                        Reactjs / ko.reactjs.org
-                      </ListGroupItemHeading>
-                    </ListGroupItemCustomTitle>
-                    <ListGroupItemCustomContent>
-                      <ListGroupItemHeading>
-                        Change sentence on hook-intro
-                        <BadgeMerged>Merged</BadgeMerged>
-                      </ListGroupItemHeading>
-                      <ListGroupItemText>
-                        <a
-                          href="https://github.com/reactjs/ko.reactjs.org/pull/188"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          view it on github
-                        </a>
-                      </ListGroupItemText>
-                    </ListGroupItemCustomContent>
-                  </ListGroup>
-                  &nbsp;
-                </Col>
-              </Row>
-            </Grid>
-          </div>
-        </div>
-      </>
-    )
+const data = [
+  {
+    repositoryName: "MochaJS",
+    contributionSets: [
+      {
+        title:
+          "Add a description for installing GraphicsMagick or ImageMagick in docs/README.md",
+        uri: "https://github.com/mochajs/mocha/pull/4045",
+        reviewStatus: "",
+        prStatus: "",
+        releaseStatus: ""
+      },
+      {
+        title: "Change sh to bash for code block in docs/index.md",
+        uri: "https://github.com/mochajs/mocha/pull/4066",
+        reviewStatus: "",
+        prStatus: "",
+        releaseStatus: ""
+      }
+    ]
+  },
+  {
+    repositoryName: "Reactjs / ko.reactjs.org",
+    contributionSets: [
+      {
+        title: "Change sentence on hook-intro",
+        uri: "https://github.com/reactjs/ko.reactjs.org/pull/188",
+        reviewStatus: "",
+        prStatus: "",
+        releaseStatus: ""
+      }
+    ]
   }
+]
+
+const ContentWrapper = ({ repositoryName, contributionSets }) => {
+  return (
+    <Col xs={12} md={12} lg={6} xl={6}>
+      <ListGroup className="mt-3">
+        <ListGroupItemCustomTitle>
+          <ListGroupItemHeading>{repositoryName}</ListGroupItemHeading>
+        </ListGroupItemCustomTitle>
+
+        {contributionSets.map(contribution => {
+          return (
+            <ContributionContent
+              title={contribution.title}
+              uri={contribution.uri}
+              reviewStatus={contribution.reviewStatus}
+              prStatus={contribution.prStatus}
+              releaseStatus={contribution.releaseStatus}
+            />
+          )
+        })}
+      </ListGroup>
+    </Col>
+  )
+}
+
+const ContributionContent = props => {
+  const { title, uri, reviewStatus, prStatus, releaseStatus } = props
+  return (
+    <ListGroupItemCustomContent>
+      <ListGroupItemHeading>
+        {title}
+
+        {reviewStatus && (
+          <BadgeCustom type={reviewStatus}>{reviewStatus}</BadgeCustom>
+        )}
+        {prStatus && <BadgeCustom type={prStatus}>{prStatus}</BadgeCustom>}
+        {releaseStatus && (
+          <BadgeCustom type={releaseStatus}>{releaseStatus}</BadgeCustom>
+        )}
+      </ListGroupItemHeading>
+      <ListGroupItemText>
+        <a href={uri} target="_blank" rel="noopener noreferrer">
+          view it on github
+        </a>
+      </ListGroupItemText>
+    </ListGroupItemCustomContent>
+  )
+}
+
+const Contribution = props => {
+  return (
+    <>
+      <div className="bg-grey pb-9">
+        <div className="bg-grey">
+          <div className="title-out pt-4 pb-4">
+            <div className="title-except">
+              <ScrollableAnchor id={"contribution"}>
+                <h1 className="font-5 font-new">contribution</h1>
+              </ScrollableAnchor>
+            </div>
+          </div>
+          <Grid>
+            <Row className="h100percent mt-9">
+              {data.map(datum => (
+                <ContentWrapper
+                  repositoryName={datum.repositoryName}
+                  contributionSets={datum.contributionSets}
+                />
+              ))}
+            </Row>
+          </Grid>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Contribution
