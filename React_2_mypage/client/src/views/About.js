671276de193404e4c51c52a1679c39de1b380d88
HyunSangHan <gustkd3@gmail.com>
2019-12-07 10:37:07 +0900

    Improve About.js structure

--- React_2_mypage/client/src/views/About.js
+++ React_2_mypage/client/src/views/About.js
@@ -9,6 +9,21 @@ import photoLinkedIn from "../images/linkedin.png"
 import photoFacebook from "../images/facebook.png"
 import photoInsta from "../images/instagram.png"
 
+const myName = "hyunsang han"
+const myJob = "Developer"
+const introduceMyself =
+  "I always try to move forward with new learning. I work as a product manager, but I take my own personal time to develop toy projects and to contribute to open source projects. If you have a good idea for a new try, feel free to contact me anytime."
+const outLinks = [
+  ["github", photoGithub, "https://github.com/HyunSangHan"],
+  [
+    "linked-in",
+    photoLinkedIn,
+    "https://www.linkedin.com/in/%ED%98%84%EC%83%81-%ED%95%9C-971604178"
+  ],
+  ["facebook", photoFacebook, "https://www.facebook.com/gustkd3"],
+  ["instagram", photoInsta, "https://www.instagram.com/phenom_0901"]
+]
+
 class About extends Component {
   render() {
     return (
@@ -33,60 +48,26 @@ class About extends Component {
                 </Col>
                 <Col className="skin" xs={12} md={6}>
                   <div className="align-left ml-5 mr-5 mb-more for-mobile">
-                    <h2 className="upper">hyunsang han</h2>
-                    <h3 className="mt-3 mb-5">Developer</h3>
-                    <h4>
-                      I always try to move forward with new learning. I work as
-                      a product manager, but I take my own personal time to
-                      develop toy projects and to contribute to open source
-                      projects. If you have a good idea for a new try, feel free
-                      to contact me anytime.
-                    </h4>
+                    <h2 className="upper">{myName}</h2>
+                    <h3 className="mt-3 mb-5">{myJob}</h3>
+                    <h4>{introduceMyself}</h4>
                     <div className="bottom">
-                      <a
-                        href="https://github.com/HyunSangHan"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        <img
-                          className="link-icon"
-                          src={photoGithub}
-                          alt="github"
-                        />
-                      </a>
-                      <a
-                        href="https://www.linkedin.com/in/%ED%98%84%EC%83%81-%ED%95%9C-971604178"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        <img
-                          className="link-icon"
-                          src={photoLinkedIn}
-                          alt="linked-in"
-                        />
-                      </a>
-                      <a
-                        href="https://www.facebook.com/gustkd3"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        <img
-                          className="link-icon"
-                          src={photoFacebook}
-                          alt="facebook"
-                        />
-                      </a>
-                      <a
-                        href="https://www.instagram.com/phenom_0901"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        <img
-                          className="link-icon"
-                          src={photoInsta}
-                          alt="instagram"
-                        />
-                      </a>
+                      {outLinks.map((outLink, idx) => {
+                        return (
+                          <a
+                            href={outLink[2]}
+                            rel="noopener noreferrer"
+                            target="_blank"
+                          >
+                            <img
+                              key={idx}
+                              className="link-icon"
+                              src={outLink[1]}
+                              alt={outLink[0]}
+                            />
+                          </a>
+                        )
+                      })}
                     </div>
                   </div>
                 </Col>
