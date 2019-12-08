4c1bfe48e5921d3af54cd56671e41c462f432a45
HyunSangHan <gustkd3@gmail.com>
2019-12-09 00:14:50 +0900

    Collect all contents to a file

--- React_2_mypage/client/src/views/About.js
+++ React_2_mypage/client/src/views/About.js
@@ -4,25 +4,7 @@ import "../css/Main.css"
 import { Grid, Row, Col } from "react-bootstrap"
 import ScrollableAnchor from "react-scrollable-anchor"
 import photoVietnam from "../images/vietnam.jpg"
-import photoGithub from "../images/github.png"
-import photoLinkedIn from "../images/linkedin.png"
-import photoFacebook from "../images/facebook.png"
-import photoInsta from "../images/instagram.png"
-
-const myName = "hyunsang han"
-const myJob = "Developer"
-const introduceMyself =
-  "I always try to move forward with new learning. I work as a product manager, but I take my own personal time to develop toy projects and to contribute to open source projects. If you have a good idea for a new try, feel free to contact me anytime."
-const outLinks = [
-  ["github", photoGithub, "https://github.com/HyunSangHan"],
-  [
-    "linked-in",
-    photoLinkedIn,
-    "https://www.linkedin.com/in/%ED%98%84%EC%83%81-%ED%95%9C-971604178"
-  ],
-  ["facebook", photoFacebook, "https://www.facebook.com/gustkd3"],
-  ["instagram", photoInsta, "https://www.instagram.com/phenom_0901"]
-]
+import { baseInfo } from "../content"
 
 class About extends Component {
   render() {
@@ -48,22 +30,22 @@ class About extends Component {
                 </Col>
                 <Col className="skin" xs={12} md={6}>
                   <div className="align-left ml-5 mr-5 mb-more for-mobile">
-                    <h2 className="upper">{myName}</h2>
-                    <h3 className="mt-3 mb-5">{myJob}</h3>
-                    <h4>{introduceMyself}</h4>
+                    <h2 className="upper">{baseInfo.name}</h2>
+                    <h3 className="mt-3 mb-5">{baseInfo.job}</h3>
+                    <h4>{baseInfo.introduceMyself}</h4>
                     <div className="bottom">
-                      {outLinks.map((outLink, idx) => {
+                      {baseInfo.snsLinks.map((sns, idx) => {
                         return (
                           <a
                             key={idx}
-                            href={outLink[2]}
+                            href={sns.uri}
                             rel="noopener noreferrer"
                             target="_blank"
                           >
                             <img
                               className="link-icon"
-                              src={outLink[1]}
-                              alt={outLink[0]}
+                              src={sns.logo}
+                              alt={sns.name}
                             />
                           </a>
                         )
