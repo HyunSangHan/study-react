bd90635795ff07e6af8a6022ed7e4bcb24b4f2a0
HyunSangHan <gustkd3@gmail.com>
2020-03-08 17:14:33 +0900

    Improve ad style

--- React_2_mypage/client/src/views/VisitBlog.js
+++ React_2_mypage/client/src/views/VisitBlog.js
@@ -1,4 +1,14 @@
 import React from "react"
+import backgroudImg from "../images/coding.jpg"
+
+const fitCoverImage = imgName => {
+  return {
+    backgroundImage: "url(" + imgName + ")",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat"
+  }
+}
 
 const VisitBlog = ({ id, className }) => {
   return (
@@ -8,7 +18,10 @@ const VisitBlog = ({ id, className }) => {
       rel="noopener noreferrer"
     >
       <div id={id} className={className}>
-        Why don’t you visit my blog?
+        <div style={fitCoverImage(backgroudImg)} className="w-100 h-100">
+          <div className="ad-frontground"></div>
+          <div className="ad-font">Why don’t you visit my blog?</div>
+        </div>
       </div>
     </a>
   )
