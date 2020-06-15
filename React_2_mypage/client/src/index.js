22cf89043a25dc5aa6033364e26fba35e446ffd6
HyunSangHan <gustkd3@gmail.com>
2020-06-16 08:03:40 +0900

    Add Google Analytics

--- React_2_mypage/client/src/index.js
+++ React_2_mypage/client/src/index.js
@@ -5,6 +5,9 @@ import "./index.css"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
 import "bootstrap/dist/css/bootstrap.css"
+import ReactGA from "react-ga"
+ReactGA.initialize("UA-167500844-1")
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 ReactDOM.render(<App />, document.getElementById("root"))
 
